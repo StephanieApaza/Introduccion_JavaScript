b85fdd1ee887d7e7c02980487bcfbfda0dea73e3
Stephanie Apaza <stef.53apza@gmail.com>
2025-07-10 19:24:20 -0400

    Tipos de datos JS

---
+++ Practica_1/tipos-de-dato.js
@@ -0,0 +1,23 @@
+// Números
+console.log(typeof 42);
+console.log(typeof -666);
+console.log(typeof 0);
+
+// Cadenas de texto
+console.log(typeof 'Veinticinco');
+console.log(typeof '');
+console.log(typeof 'Hola Mundo')
+
+// Booleanos
+console.log(typeof true);
+console.log(typeof false);
+
+// Otros valores
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof [5, 4, 7]);
+console.log(typeof {clase: 'matemática'});
+console.log(typeof ('lunes','martes','jueves'));
+console.log(typeof (5, 10, 55, 4));
+console.log(typeof '>=')
